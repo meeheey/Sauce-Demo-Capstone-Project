5256cf878637407e9833dfac44bf6b68e1a3b511
meeheey <m.m.zeon@gmail.com>
2025-12-18 15:59:23 +0100

    Complete FooterLinksTest

--- src/test/java/Tests/FooterLinksTest.java
+++ src/test/java/Tests/FooterLinksTest.java
@@ -1,6 +1,9 @@
 package Tests;
 
 import Base.BaseTest;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -19,10 +22,41 @@ public class FooterLinksTest extends BaseTest {
     @Test
     public void userCanGoToX() {
         footer.clickOnXLink();
+        ArrayList<String> tabList = new ArrayList<>(driver.getWindowHandles());
+        Assert.assertTrue(isUrlInTabs(tabList, "https://x.com/saucelabs"));
+        }
+
+    @Test
+    public void userCanGoToFacebook() {
         footer.clickOnFacebookLink();
+        ArrayList<String> tabList = new ArrayList<>(driver.getWindowHandles());
+        Assert.assertTrue(isUrlInTabs(tabList, "https://www.facebook.com/saucelabs"));
+    }
+
+    @Test
+    public void userCanGoToLinkedIn() {
         footer.clickOnLinkedInLink();
         ArrayList<String> tabList = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(tabList.get(1));
-        
+        Assert.assertTrue(isUrlInTabs(tabList, "https://www.linkedin.com/company/sauce-labs/"));
+    }
+
+    public boolean isUrlInTabs(ArrayList<String> tabList, String url) {
+        for (String tab : tabList) {
+            driver.switchTo().window(tab);
+            wait.until((WebDriver d) -> {
+                try {
+                    return ((JavascriptExecutor) d)
+                            .executeScript("return document.readyState")
+                            .equals("complete");
+                } catch (Exception e) {
+                    return false;
+                }
+            });
+           if (driver.getCurrentUrl().equals(url)) {
+               return true;
+           }
+           driver.close();
+        }
+        return false;
     }
 }
