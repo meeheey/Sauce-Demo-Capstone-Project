a2abba363d291c4b21a7722fe2442493913dac38
meeheey <m.m.zeon@gmail.com>
2025-12-22 11:51:51 +0100

    Finish Reset App State Link Test

--- src/test/java/Tests/MenuTest.java
+++ src/test/java/Tests/MenuTest.java
@@ -2,6 +2,7 @@ package Tests;
 
 import Base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -15,11 +16,11 @@ public class MenuTest extends BaseTest {
         loginPage.inputPassword("secret_sauce");
         loginPage.clickOnSubmitButton();
         wait.until(ExpectedConditions.urlContains("inventory"));
-        homePage.clickOnMenuButton();
     }
 
     @Test
     public void allItemsTest() {
+        homePage.clickOnMenuButton();
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("inventory_sidebar_link"))));
         menu.clickOnAllItemsLink();
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
@@ -27,6 +28,7 @@ public class MenuTest extends BaseTest {
 
     @Test
     public void aboutLinkTest() {
+        homePage.clickOnMenuButton();
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("about_sidebar_link"))));
         menu.clickOnAboutLink();
         wait.until(ExpectedConditions.urlToBe("https://saucelabs.com/"));
@@ -35,6 +37,7 @@ public class MenuTest extends BaseTest {
 
     @Test
     public void logoutLinkTest() {
+        homePage.clickOnMenuButton();
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("logout_sidebar_link"))));
         menu.clickOnLogoutLink();
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
@@ -42,9 +45,15 @@ public class MenuTest extends BaseTest {
 
     @Test
     public void resetAppStateLinkTest() {
+        for (WebElement button : homePage.getInventoryItemButtons()){
+            button.click();
+        }
+        homePage.clickOnMenuButton();
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("reset_sidebar_link"))));
         menu.clickOnResetAppStateLink();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
-        // TODO
+        driver.navigate().refresh();
+        for (WebElement button : homePage.getInventoryItemButtons()){
+            Assert.assertEquals(button.getText(), "Add to cart");
+        }
     }
 }
